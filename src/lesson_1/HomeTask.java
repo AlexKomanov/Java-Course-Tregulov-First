1351ed6e252154722041411bce42cdb33e8cb308
Alex Komanov <akomanov@seatgeek.com>
2020-08-21 06:32:06 +0300

    First Lesson - Added Home Work

---
+++ src/lesson_1/HomeTask.java
@@ -0,0 +1,19 @@
+package lesson_1;
+
+/**
+ * @author Alex Komanov
+ */
+public class HomeTask {
+    public static void main(String[] args) {
+
+        System.out.println("               Lorem Ipsum");
+        System.out.println("Neque porro quisquam est qui dolorem ipsum quia");
+        System.out.println("Dolor sit amet, consectetur, adipisci velit...");
+        System.out.println("There is no one who loves pain itself, who seeks after it");
+        System.out.println("and wants to have it, simply because it is pain...");
+        System.out.println("                                                   Lorem Ipsum");
+        /*
+" "
+" "*/
+    }
+}
