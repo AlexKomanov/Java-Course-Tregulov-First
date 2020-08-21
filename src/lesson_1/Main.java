f95699bb037aca369b683d1af22f526ee87afb61
Alex Komanov <akomanov@seatgeek.com>
2020-08-21 06:24:53 +0300

    First Lesson - Methods of printing

---
+++ src/lesson_1/Main.java
@@ -0,0 +1,22 @@
+package lesson_1;
+
+/**
+ * @author Alex Komanov
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("A ");
+        System.out.println("a");
+        System.out.print("B ");
+        System.out.println("b");
+        System.out.print("C ");
+        System.out.println("c");
+
+        System.out.println();
+        System.out.println("abcdef");
+        System.out.println();
+        System.out.println("ghijkl");
+
+    }
+}
